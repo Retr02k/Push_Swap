9d07026b6f748190a33f55145fa0c3cbfb12af1a
Retr02k <pedro210332@gmail.com>
2026-02-16 19:30:33 +0000

    efactor(push_a): improve code readability with descriptive variables
    - Introduced 'second_node' and 'last_node' variables to replace complex
      pointer arithmetic (stack_b->head->next->prev)
    - Made the logic of disconnecting nodes from stack_b more explicit
    - Maintained same functionality while improving code clarity

--- src/moves/pa.c
+++ src/moves/pa.c
@@ -6,7 +6,7 @@
 /*   By: psilva-p <psilva-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 18:06:27 by psilva-p          #+#    #+#             */
-/*   Updated: 2026/02/16 17:43:05 by psilva-p         ###   ########.fr       */
+/*   Updated: 2026/02/16 18:48:06 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,27 @@
 void	push_a(t_stack *stack_a, t_stack *stack_b)
 {
 	t_node	*tmp;
-	t_node	*tmp2;
 	t_node	*second_node;
+	t_node	*last_node;
 
 	if (stack_b->head == NULL)
 		return ;
+	tmp = stack_b->head;
+	if (tmp->next == tmp)
+	{
+		stack_b->head = NULL;
+		add_node_to_stack(stack_a, tmp);
+		stack_b->size--;
+		stack_a->size++;
+	}
 	else
 	{
-		tmp = stack_b->head;
-		tmp2 = stack_a->head;
 		second_node = stack_b->head->next;
-		if (tmp->next == tmp)
-		{
-			stack_b->head = NULL;
-			stack_a->head = tmp;
-			stack_a->head->prev = tmp2->prev;
-			tmp2->prev->next = tmp2;
-			stack_b->size--;
-			stack_a->size++;
-			return ;
-		}
+		last_node = stack_b->head->prev;
+		second_node->prev = last_node;
+		last_node->next = second_node;
 		stack_b->head = second_node;
-		stack_b->head->prev = tmp->prev;
-		tmp->prev->next = stack_b->head;
-		if (stack_a->head == NULL)
-		{
-			stack_a->head = tmp;
-		}
-		stack_a->head = tmp;
-		stack_a->head->prev = tmp2->prev;
-		tmp2->prev->next = tmp2;
+		add_node_to_stack(stack_a, tmp);
 		stack_b->size--;
 		stack_a->size++;
 	}
